5014de4c1829cd70a1910f10b15f12b7b1b9aef4
Alvaro Agea <alvaro.agea@gmail.com>
2014-02-26 12:17:14 +0100

    Rename main

---
+++ src/main/scala/org/cronos/server/main.scala
@@ -0,0 +1,31 @@
+package org.cronos.server
+
+import akka.actor.{Props, ActorSystem}
+import scala.annotation.tailrec
+import org.cronos.command.CommandExecutorActor
+
+
+object Main extends App with ConfigCassandra {
+  import ConsoleCommands._
+  implicit lazy val system = ActorSystem()
+  val commandExecutor =  system.actorOf(Props(new CommandExecutorActor()))
+
+  @tailrec
+  private def commandLoop(): Unit = {
+    Console.readLine() match {
+      case QuitCommand  => return
+      case x            => commandExecutor ! x
+    }
+    commandLoop()
+  }
+
+  commandLoop()
+
+  system.shutdown()
+}
+
+object ConsoleCommands {
+  val QuitCommand   = "quit"
+}
+
+
